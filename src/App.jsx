d897202b93da7cfbdc997ab97cf8369e29457ac2
Digv06 <86612866+Digv06@users.noreply.github.com>
2023-03-31 13:34:07 +0530

    Project Created and Header is added.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Container, Box } from "@chakra-ui/react";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+      <Box bg="blue.600" color={"white"} height="100vh" paddingTop={130}>
+        <Container maxW="3xl" centerContent>
+          <Header />
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+export default App;
